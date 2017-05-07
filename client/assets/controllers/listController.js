8cdc8e70bd88886dfbaf6a8c56cddaec5d521b91
Ryan Pan <purexchaos@gmail.com>
2017-05-06 20:31:59 -0700

    added comment feature

--- client/assets/controllers/listController.js
+++ client/assets/controllers/listController.js
@@ -1,15 +1,49 @@
-app.controller('ListController', ['$scope', ListController])
+app.controller('ListController', ['$scope', '$rootScope', ListController])
 
-function ListController(sc){
+function ListController(sc, rs){
+    rs.name = "Admin"
     sc.itemList = [];
-    sc.addItem = function(){
-        console.log(sc.item.name);
-        var item = {
-            name: sc.item.name,
-            desc: sc.item.desc,
-            completed: true,
+    sc.view = "list";
+    sc.currItemIndex;
+
+    function clearFields(obj){
+        for(key in obj){
+            obj[key] = "";
         }
-        sc.itemList.push(item);
-        sc.item = {};
+    }
+
+    sc.addItem = function(item){
+        var date = new Date();
+        var itemToAdd = {
+            name: item.name,
+            desc: item.desc,
+            date: date,
+            status: "New",
+            completed: true,
+            comments: []
+        };
+        sc.itemList.push(itemToAdd);
+        clearFields(item);
+    }
+
+    sc.returnToList = function(){
+        sc.view = "list";
+    }
+
+    sc.toComments = function(index){
+        console.log(index);
+        sc.currItemIndex = index;
+        sc.view = "comments";
+    }
+
+    sc.addComment = function(comment){
+        var date = new Date();
+        var commentToAdd = {
+            user: rs.name,
+            text: comment.text,
+            date: date
+        }
+        sc.itemList[sc.currItemIndex].comments.push(commentToAdd);
+        clearFields(comment);
     }
 }
